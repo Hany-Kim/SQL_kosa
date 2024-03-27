5d83006302d4a87b5731366b25ffc2777e24b137
Hany-Kim <74503250+Hany-Kim@users.noreply.github.com>
2024-03-27 18:05:42 +0900

    Update Oracle-21c.sql

--- DAY_24_03_27/Oracle-21c.sql
+++ DAY_24_03_27/Oracle-21c.sql
@@ -8,4 +8,40 @@ SELECT first_name, job_id, hire_date
 FROM employees
 WHERE job_id = (SELECT job_id
                 FROM employees
-                WHERE employee_id=103);    
+                WHERE employee_id=103);
+
+SELECT first_name, salary
+FROM employees
+WHERE salary > ANY (SELECT salary
+                FROM employees
+                WHERE first_name='David');
+                
+SELECT first_name, department_id, job_id
+FROM employees
+WHERE department_id IN (SELECT department_id
+                        FROM employees
+                        WHERE first_name='David');    
+                        
+SELECT first_name, department_id, job_id
+FROM employees e
+WHERE EXISTS (SELECT *
+                FROM departments d
+                WHERE d.manager_id=e.employee_id);
+                
+SELECT first_name, e.department_id,job_id
+FROM departments d
+JOIN employees e
+ON d.manager_id=e.employee_id
+ORDER BY e.department_id;
+
+SELECT first_name, salary
+FROM employees a
+WHERE salary > (SELECT avg(salary)
+                FROM employees b
+                WHERE b.department_id=a.department_id);
+                
+SELECT first_name, (SELECT department_name
+    FROM departments d
+    WHERE d.department_id=e.department_id) department_name
+FROM employees e
+ORDER BY first_name;
